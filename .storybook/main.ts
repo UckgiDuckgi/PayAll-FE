f107734e220df5c50a869a1ec9228a3d6f3ac9ce
Sionparadox <sion9999@naver.com>
2025-01-13 15:12:05 +0900

    [test] 🏀 스토리북 초기 세팅 및 일부 컴포넌트 적용

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,19 +2,23 @@ import type { StorybookConfig } from '@storybook/nextjs';
 
 const config: StorybookConfig = {
   stories: [
-    '../stories/**/*.mdx',
     '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)',
+    '../stories/**/**/*.stories.@(js|jsx|mjs|ts|tsx)',
   ],
   addons: [
     '@storybook/addon-onboarding',
     '@storybook/addon-essentials',
     '@chromatic-com/storybook',
     '@storybook/addon-interactions',
+    '@storybook/addon-styling',
   ],
   framework: {
     name: '@storybook/nextjs',
     options: {},
   },
   staticDirs: ['../public'],
+  docs: {
+    autodocs: 'tag',
+  },
 };
 export default config;
