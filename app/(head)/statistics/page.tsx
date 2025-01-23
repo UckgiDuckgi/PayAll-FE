1ed15be6ef21a537af00478647de159c1e068bd8
hhbb0081 <likesun2000@naver.com>
2025-01-23 13:48:20 +0900

    [feat] 🍀소비분석 페이지 예외처리

--- app/(head)/statistics/page.tsx
+++ app/(head)/statistics/page.tsx
@@ -9,6 +9,7 @@ import { CATEGORY } from '@/constants/category';
 import { COLORS } from '@/constants/color';
 import { QUERY_KEYS } from '@/constants/queryKey';
 import { useGenericQuery } from '@/hooks/query/globalQuery';
+import { Triangle } from '@/public/icons/Triangle';
 import { StatisticsType } from '@/types/statisticsType';
 import { Category } from '@/types/table';
 import dayjs from 'dayjs';
@@ -29,13 +30,56 @@ const TitleLine = ({ title }: { title: string }) => {
   );
 };
 
+const DateStepper = ({ date }: { date: string }) => {
+  const prevDate = dayjs(date).subtract(1, 'M').format('YYYY-MM');
+  const nextDate = dayjs(date).add(1, 'M').format('YYYY-MM');
+
+  // 미래인지 확인
+  const isOverDate = (date: string) => {
+    return dayjs(date).isAfter(dayjs(), 'month');
+  };
+
+  return (
+    <div className='w-full flex items-center justify-center gap-3'>
+      <Link href={`/statistics?date=${prevDate}`}>
+        <Image
+          src='/icons/triangle.svg'
+          alt='triangle'
+          width={14}
+          height={14}
+          className='cursor-pointer'
+        />
+      </Link>
+      <span className='text-[1rem] font-bold tracking-wider'>{date}</span>
+      {!isOverDate(nextDate) && (
+        <Link
+          onClick={() => isOverDate(nextDate)}
+          href={`/statistics?date=${nextDate}`}
+        >
+          <Image
+            src='/icons/triangle.svg'
+            alt='triangle'
+            width={14}
+            height={14}
+            style={{ transform: 'rotate(180deg)' }}
+            className='cursor-pointer'
+          />
+        </Link>
+      )}
+    </div>
+  );
+};
+
 function StatisticsContent() {
   const searchParams = useSearchParams();
+  const date = useMemo(
+    () => searchParams.get('date') as string,
+    [searchParams]
+  );
   const category = useMemo(
     () => searchParams.get('category') as Category,
     [searchParams]
   );
-  const date = useMemo(() => searchParams.get('date'), [searchParams]);
   const defaultDate = dayjs().format('YYYY-MM');
 
   const { resData: statisticsData, isLoading } =
@@ -55,39 +99,38 @@ function StatisticsContent() {
     fixedExpenses,
   } = statisticsData.data;
 
-  console.log(statisticsData);
+  if (totalSpent === 0) {
+    return (
+      <>
+        <DateStepper date={date} />
+
+        <div className='flex flex-col justify-between items-center gap-10 pt-28 sm:pt-8 w-[90%] mx-auto'>
+          <div>
+            <div className='mx-auto w-[150px] sm:w-[200px] h-auto'>
+              <Image
+                src='/images/money.svg'
+                alt='money'
+                width={200}
+                height={200}
+                className='float-animation'
+              />
+            </div>
+            <div className='flex flex-col gap-1 text-[1.25rem] items-center font-semibold'>
+              <span>소비내역이 존재하지 않아요.</span>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  }
+
   const categoryExpenses = categories
     ?.filter((c) => c.categoryName !== 'TOTAL')
     .sort((a, b) => b.amount - a.amount);
   if (!category) {
     return (
       <div className='w-full mx-auto'>
-        <div className='w-full flex items-center justify-center gap-3'>
-          <Link
-            href={`/statistics?date=${dayjs(date).subtract(1, 'M').format('YYYY-MM')}`}
-          >
-            <Image
-              src='/icons/triangle.svg'
-              alt='triangle'
-              width={14}
-              height={14}
-              className='cursor-pointer'
-            />
-          </Link>
-          <span className='text-[1rem] font-bold tracking-wider'>{date}</span>
-          <Link
-            href={`/statistics?date=${dayjs(date).add(1, 'M').format('YYYY-MM')}`}
-          >
-            <Image
-              src='/icons/triangle.svg'
-              alt='triangle'
-              width={14}
-              height={14}
-              style={{ transform: 'rotate(180deg)' }}
-              className='cursor-pointer'
-            />
-          </Link>
-        </div>
+        <DateStepper date={date} />
 
         <CategoryChart
           categoryExpenses={categoryExpenses}
@@ -105,11 +148,21 @@ function StatisticsContent() {
           />
           <AccentText
             prefix='지난달 대비'
-            accent={`${difference?.toLocaleString() ?? 0}원`}
-            suffix='지출했어요.'
-            accentColor='text-main'
-            accentSize='text-[1.25rem]'
-            className='text-[#AAAAAA]'
+            accent={((difference ?? 0 < 0)
+              ? Math.abs(difference)
+              : difference
+            )?.toLocaleString()}
+            suffix='원 지출했어요.'
+            icon={
+              (difference ?? 0 > 0) ? (
+                <Triangle className='rotate-180' color='#FF6F6F' />
+              ) : (
+                <Triangle color='#6A8DFF' />
+              )
+            }
+            accentColor={(difference ?? 0 > 0) ? 'text-red' : 'text-main'}
+            className='text-[0.8125rem]'
+            accentSize='text-[1.375rem]'
           />
         </div>
 
@@ -143,15 +196,22 @@ function StatisticsContent() {
           <TitleLine title='고정 지출' />
 
           <ul className='space-y-3 my-5'>
-            {fixedExpenses?.map(({ fixedName, amount, dueDate }, idx) => (
-              <li key={fixedName + idx}>
-                <FixedExpensesCard
-                  expensesName={fixedName}
-                  expensesPrice={amount}
-                  date={dueDate}
-                />
-              </li>
-            ))}
+            {fixedExpenses?.length > 0 ? (
+              fixedExpenses?.map(({ fixedName, amount, dueDate }, idx) => (
+                <li key={fixedName + idx}>
+                  <FixedExpensesCard
+                    expensesName={fixedName}
+                    expensesPrice={amount}
+                    date={dueDate}
+                  />
+                </li>
+              ))
+            ) : (
+              <div className='my-3 flex items-center justify-center gap-3'>
+                <Image src='/images/pin.svg' alt='pin' width={40} height={40} />
+                <span>고정 지출이 없습니다.</span>
+              </div>
+            )}
           </ul>
         </div>
       </div>
